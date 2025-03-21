4e35685cb5458c63513016e402d6aba6d7476dae
SeolA-Lee <dlcodus1201@naver.com>
2025-03-22 00:41:22 +0900

    Computer, SmartPhone 클래스 구현

--- src/main/java/org/example/Computer.java
+++ src/main/java/org/example/Computer.java
@@ -2,9 +2,24 @@ package org.example;
 
 public class Computer extends Product{
 
+    private String cpu;
+
+    public Computer(String name, int price, String cpu) {
+        super(name, price);
+        this.cpu = cpu;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
     //TODO: return null을 지우고 구현
     @Override
     public String extraInfo() {
-        return null;
+        return String.format("cpu: %s", cpu);
     }
 }
