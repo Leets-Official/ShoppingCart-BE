e32f7a6d5b481e4fd30eb79c6f63b91a55ac4f9c
SeolA-Lee <dlcodus1201@naver.com>
2025-03-22 16:03:54 +0900

    ShopApp 클래스 start(), addItemInCart() 구현 / printProductList() 수정

--- src/main/java/org/example/ShopApp.java
+++ src/main/java/org/example/ShopApp.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -17,10 +18,41 @@ public class ShopApp {
     public void start() {
         //TODO: 구현
         printProductList();
+
+        while (true) {
+            String input = scan.next();
+            if (input.equals("q")) { // 'q' 입력 시 종료
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (input.equals("c")) { // 'c' 입력 시 장바구니 목록 출력
+                System.out.println("# 장바구니 목록 #");
+                System.out.println("================================");
+                List<Product> productList = cart.getProductList();
+
+                long amount = 0;
+                for (Product product : productList) {
+                    System.out.println(product);
+                    amount += product.getPrice();
+                }
+                System.out.print(">>>> " + amount + " 결제를 진행할까요? (y/n): ");
+
+                String pay = scan.next();
+                if (pay.equals("y")) {
+                    System.out.println(">>>> 결제가 완료 되었습니다!!");
+                    break;
+                } else {
+                    printProductList();
+                }
+            } else { // 해당 상품 담기
+                addItemInCart(input);
+                printProductList();
+            }
+        }
     }
 
     public void addItemInCart(String item) {
         //TODO: 구현
+        cart.add(Integer.parseInt(item));
     }
 
     public void printProductList() {
@@ -31,5 +63,6 @@ public class ShopApp {
             System.out.println("[" + entry.getKey() + "] " + entry.getValue());
         }
         System.out.println("--------------------------------");
+        System.out.print("제품 번호를 입력 하세요(결제:c, 종료:q): ");
     }
 }
